2eca66e43ed3f4e971b3f1f9d792149914d94f12
sewon <sewon225@gmail.com>
2022-04-29 16:34:28 +0900

    [init] 초기세팅

---
+++ src/test/java/com/firebird/hellcipe/HellcipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firebird.hellcipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellcipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
