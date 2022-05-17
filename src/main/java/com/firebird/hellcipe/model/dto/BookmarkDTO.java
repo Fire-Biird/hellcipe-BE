e74ce2630678d8cde55bc8fab75cdb041472f8f1
jobumjin <whqjawls2673@naver.com>
2022-05-17 17:36:37 +0900

    Feat: UserDTO, BookmarkDTO, Bookmark, user

--- src/main/java/com/firebird/hellcipe/model/dto/BookmarkDTO.java
+++ src/main/java/com/firebird/hellcipe/model/dto/BookmarkDTO.java
@@ -1,5 +1,26 @@
 package com.firebird.hellcipe.model.dto;
 
 public class BookmarkDTO {
-    
+    private int userId;
+    private String rcpSeq;
+
+    public BookmarkDTO() {}
+
+    public BookmarkDTO(int userId, String rcpSeq) {
+        this.userId = userId;
+        this.rcpSeq = rcpSeq;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    public String getRcpSeq() {
+        return rcpSeq;
+    }
+    public void setRcpSeq(String rcpSeq) {
+        this.rcpSeq = rcpSeq;
+    }
 }
--- src/main/java/com/firebird/hellcipe/model/dto/UserDTO.java
+++ src/main/java/com/firebird/hellcipe/model/dto/UserDTO.java
@@ -1,5 +1,44 @@
 package com.firebird.hellcipe.model.dto;
 
 public class UserDTO {
+    private String userNickname;
+    private String userPassword;
+    private String userEmail;
+    private String userId;
+
+    public UserDTO() {}
+
+    public UserDTO(String userNickname, String userPassword, String userEmail, String userId) {
+        this.userNickname = userNickname;
+        this.userPassword = userPassword;
+        this.userEmail = userEmail;
+        this.userId = userId;
+    }
+    public String getUserNickname() {
+        return userNickname;
+    }
+    public void setUserNickname(String userNickname) {
+        this.userNickname = userNickname;
+    }
+    public String getUserPassword() {
+        return userPassword;
+    }
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+    public String getUserEmail() {
+        return userEmail;
+    }
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
     
+
 }
